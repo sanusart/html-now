4b27518c7e140fb1bdd7ae8cc6002c234f321dea
Sasha Khamkov <sanusart@gmail.com>
2014-05-23 09:54:07 +0300

    FEATURE: libraries now pulled from data json
    - libraries now pulled from data json to help maintain versions and libs
    - add more libraries (Angular)

--- app/js/directives.js
+++ app/js/directives.js
@@ -6,37 +6,30 @@ angular.module('htmlNow.directives', []).directive('tplCode', ['dataService', '$
     return {
         restrict: "E",
         templateUrl: 'partials/template.html',
-        /*scope: {
-         type: '=',
-         name: '=',
-         title: '=tpl_html_title'
-         },*/
         link: function (scope, element, attrs) {
 
             scope.tpl_js = [];
             scope.tpl_css = [];
 
-            scope.delete = function (index,delete_type) {
-                (delete_type==='css') ? scope.tpl_css.splice(index, 1) : scope.tpl_js.splice(index, 1);
+            scope.delete = function (index, delete_type) {
+                (delete_type === 'css') ? scope.tpl_css.splice(index, 1) : scope.tpl_js.splice(index, 1);
             };
 
-            scope.$watchCollection('[type,name]', function (newValue) {
-                var lib = {
-                    type: newValue[0],
-                    name: newValue[1]
-                };
+            scope.$watchCollection('id', function (newValue) {
+                var id = newValue;
+                dataService.getLibs(function (libList) {
 
-                if (lib.type === 'js') {
-                    scope.tpl_js.push('<script data-head-script="' + $filter('noSpace')(lib.name) + '" src="' + $filter('setVersion')(dataService[lib.type][lib.name].url, dataService[lib.type][lib.name].version) + '"></script>');
-                    if (lib.name === 'modernizr') {
-                        scope.tpl_tag_html = '<!--[if lt IE 7]><html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->\n<!--[if IE 7]><html lang="en" class="no-js lt-ie9 lt-ie8"><![endif]-->\n<!--[if IE 8]><html lang="en" class="no-js lt-ie9"><![endif]-->\n<!--[if gt IE 8]><!--><html lang="en" class="no-js"><!--<![endif]-->';
+                    if (libList.data[id]['type'] === 'js') {
+                        scope.tpl_js.push('<script data-head-script="' + $filter('noSpace')(libList.data[id]['name']) + '" src="' + $filter('setVersion')(libList.data[id]['url'], libList.data[id]['version']) + '"></script>');
+                        if (libList.data[id]['name'] === 'modernizr') {
+                            scope.tpl_tag_html = '<!--[if lt IE 7]><html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->\n<!--[if IE 7]><html lang="en" class="no-js lt-ie9 lt-ie8"><![endif]-->\n<!--[if IE 8]><html lang="en" class="no-js lt-ie9"><![endif]-->\n<!--[if gt IE 8]><!--><html lang="en" class="no-js"><!--<![endif]-->';
+                        }
+                    } else if (libList.data[id]['type'] === 'css') {
+                        scope.tpl_css.push('<link rel="stylesheet" data-head-script="' + $filter('noSpace')(libList.data[id]['name']) + '" href="' + $filter('setVersion')(libList.data[id]['url'], libList.data[id]['version']) + '">');
+                    } else {
+                        console.log('We still don\'t know how to process ' + libList.data[id]['type'] + ' :(');
                     }
-                } else if (lib.type === 'css') {
-                    scope.tpl_css.push('<link rel="stylesheet" data-head-script="' + $filter('noSpace')(lib.name) + '" href="' + $filter('setVersion')(dataService[lib.type][lib.name].url, dataService[lib.type][lib.name].version) + '">');
-                } else {
-                    console.log('We still don\'t know how to process ' + lib.type + ' :(');
-                }
-                //console.log('scope.tpl_js',scope.tpl_js);
+                });
             });
         }
     }
