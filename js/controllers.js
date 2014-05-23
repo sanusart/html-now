42d55517ffae619b7ad2343bc28fc67729b08132
Sasha Khamkov <sanusart@gmail.com>
2014-05-23 09:54:07 +0300

    FEATURE: libraries now pulled from data json
    - libraries now pulled from data json to help maintain versions and libs
    - add more libraries (Angular)

--- js/controllers.js
+++ js/controllers.js
@@ -3,14 +3,17 @@
 /* Controllers */
 
 angular.module('htmlNow.controllers', [])
-    .controller('Main', ['$scope', function ($scope) {
+    .controller('Main', ['$scope','dataService', function ($scope,dataService) {
         $scope.welcome = 'Hello, Hello';
         $scope.tpl_html_title ='My template';
         $scope.tpl_body_text = '<p>This is <b>my</b> text.</p>';
         $scope.tpl_meta_description = 'My template';
         $scope.tpl_meta_keywords = 'My, template, keywords';
-        $scope.useScript = function (type, name) {
-            $scope.type = type;
-            $scope.name = name;
+        dataService.getLibs(function(libraries){
+            $scope.libs = libraries.data;
+            console.log('[------------------- libs ]',$scope.libs);
+        });
+        $scope.useScript = function (index) {
+            $scope.id = index;
         };
     }]);
--- js/services.js
+++ js/services.js
@@ -3,37 +3,9 @@
 /* Services */
 
 angular.module('htmlNow.services', []).factory('dataService', function ($http) {
-    var libs = {
-        "js": {
-            "jquery v1": {
-                "url": "//cdn.jsdelivr.net/jquery/###/jquery.js",
-                "version": "1.11.1"
-            },
-            "jquery-migrate": {
-                "url": "//cdn.jsdelivr.net/jquery.migrate/###/jquery-migrate.js",
-                "version": "1.2.1"
-            },
-            "jquery v2": {
-                "url": "//cdn.jsdelivr.net/jquery/###/jquery.js",
-                "version": "2.1.1"
-            },
-            "modernizr": {
-                "url": "//cdn.jsdelivr.net/modernizr/###/modernizr.js",
-                "version": "2.8.1"
-            },
-            "local-file": {
-                "url": "./js/script.js"
-            }
-        },
-        "css": {
-            "normalize": {
-                "url": "//cdn.jsdelivr.net/normalize/###/normalize.css",
-                "version": "3.0.1"
-            },
-            "local-file": {
-                "url": "./style/style.css"
-            }
+    return {
+        getLibs: function (libs) {
+            $http.get('./data/libs.json').then(libs);
         }
-    };
-    return libs;
+    }
 });
