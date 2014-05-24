ac93871bcfcbebf45688957e6d9f690c6b07844a
Sasha Khamkov <sanusart@gmail.com>
2014-05-24 23:28:06 +0300

    FEATURE: add search to scripts list
    - add search to scripts list
    - small style changes

--- js/controllers.js
+++ js/controllers.js
@@ -3,17 +3,17 @@
 /* Controllers */
 
 angular.module('htmlNow.controllers', [])
-    .controller('Main', ['$scope','dataService', function ($scope,dataService) {
+    .controller('Main', ['$scope', 'dataService', function ($scope, dataService) {
         $scope.welcome = 'Hello, Hello';
-        $scope.tpl_html_title ='My template';
+        $scope.tpl_html_title = 'My template';
         $scope.tpl_body_text = '<p>This is <b>my</b> text.</p>';
         $scope.tpl_meta_description = 'My template';
         $scope.tpl_meta_keywords = 'My, template, keywords';
-        dataService.getLibs(function(libraries){
+        dataService.getLibs(function (libraries) {
             $scope.libs = libraries.data;
-            console.log('[------------------- libs ]',$scope.libs);
         });
-        $scope.useScript = function (index) {
-            $scope.id = index;
+
+        $scope.useScript = function (name) {
+            $scope.id = name;
         };
     }]);
--- js/directives.js
+++ js/directives.js
@@ -10,25 +10,48 @@ angular.module('htmlNow.directives', []).directive('tplCode', ['dataService', '$
 
             scope.tpl_js = [];
             scope.tpl_css = [];
+            scope.tpl_meta = [];
 
             scope.delete = function (index, delete_type) {
-                (delete_type === 'css') ? scope.tpl_css.splice(index, 1) : scope.tpl_js.splice(index, 1);
+                switch (delete_type) {
+                    case 'css':
+                        scope.tpl_css.splice(index, 1);
+                        break;
+                    case 'js':
+                        scope.tpl_js.splice(index, 1);
+                        break;
+                    case 'meta':
+                        scope.tpl_meta.splice(index, 1);
+                        break;
+                    default:
+                        throw new Error('Nothing specified, nothing to delete.');
+                }
             };
 
-            scope.$watchCollection('id', function (newValue) {
-                var id = newValue;
+            scope.$watchCollection('id', function (newValue, oldValue) {
+                var script_name = newValue;
                 dataService.getLibs(function (libList) {
 
-                    if (libList.data[id]['type'] === 'js') {
-                        scope.tpl_js.push('<script data-head-script="' + $filter('noSpace')(libList.data[id]['name']) + '" src="' + $filter('setVersion')(libList.data[id]['url'], libList.data[id]['version']) + '"></script>');
-                        if (libList.data[id]['name'] === 'modernizr') {
-                            scope.tpl_tag_html = '<!--[if lt IE 7]><html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->\n<!--[if IE 7]><html lang="en" class="no-js lt-ie9 lt-ie8"><![endif]-->\n<!--[if IE 8]><html lang="en" class="no-js lt-ie9"><![endif]-->\n<!--[if gt IE 8]><!--><html lang="en" class="no-js"><!--<![endif]-->';
+                    if (script_name === oldValue) return;
+
+                    angular.forEach(libList.data, function (val) {
+
+                        if (val.name === script_name) {
+
+                            if (val.type === 'js') {
+                                scope.tpl_js.push('<script src="' + $filter('setVersion')(val.url, val.version) + '"></script>');
+                                if (val.name === 'Modernizr') {
+                                    scope.tpl_tag_html = '<!--[if lt IE 7]><html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->\n<!--[if IE 7]><html lang="en" class="no-js lt-ie9 lt-ie8"><![endif]-->\n<!--[if IE 8]><html lang="en" class="no-js lt-ie9"><![endif]-->\n<!--[if gt IE 8]><!--><html lang="en" class="no-js"><!--<![endif]-->';
+                                }
+                            } else if (val.type === 'css') {
+                                scope.tpl_css.push('<link rel="stylesheet" href="' + $filter('setVersion')(val.url, val.version) + '">');
+                            } else {
+                                console.log('We still don\'t know how to process ' + val.type + ' :(');
+                            }
+
                         }
-                    } else if (libList.data[id]['type'] === 'css') {
-                        scope.tpl_css.push('<link rel="stylesheet" data-head-script="' + $filter('noSpace')(libList.data[id]['name']) + '" href="' + $filter('setVersion')(libList.data[id]['url'], libList.data[id]['version']) + '">');
-                    } else {
-                        console.log('We still don\'t know how to process ' + libList.data[id]['type'] + ' :(');
-                    }
+
+                    });
                 });
             });
         }
