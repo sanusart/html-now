f36fde5f03bdcc7e060878a99013a3b6333e512a
Sasha Khamkov <sanusart@gmail.com>
2014-06-04 14:38:39 +0300

    FEATURES, CLEAN-UP, STYLE1: several improvements and features
    - fix UI
    - add more libraries
    - add `bundles` - creates template with predefined scripts
    - get rid of `<details>` - browser support is very poor, use dl,dt,dd and ng-show instead
    - update README.md
    - clean-up

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
 'use strict';
 
-
-// Declare app level module which depends on filters, and services
 angular.module('htmlNow', [
-  'ngRoute',
-  'htmlNow.filters',
-  'htmlNow.services',
-  'htmlNow.directives',
-  'htmlNow.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'Main'});
-  $routeProvider.otherwise({redirectTo: '/'});
+    'ngRoute',
+    'htmlNow.filters',
+    'htmlNow.services',
+    'htmlNow.directives',
+    'htmlNow.controllers'
+]).config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/:names?', {templateUrl: 'partials/main.html', controller: 'Main'});
+    $routeProvider.otherwise({redirectTo: '/'});
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -2,18 +2,33 @@
 
 /* Controllers */
 
-angular.module('htmlNow.controllers', [])
-    .controller('Main', ['$scope', 'dataService', function ($scope, dataService) {
+angular.module('htmlNow.controllers', ['ngRoute'])
+    .controller('Main', ['$scope', 'dataService', '$route', '$filter', '$timeout', '$location', '$window', function ($scope, dataService, $route, $filter, $timeout, $location, $window) {
         $scope.welcome = 'Hello, Hello';
         $scope.tpl_html_title = 'My template';
         $scope.tpl_body_text = '<p>This is <b>my</b> text.</p>';
         $scope.tpl_meta_description = 'My template';
-        $scope.tpl_meta_keywords = 'My, template, keywords';
+        $scope.date = new Date();
         dataService.getLibs(function (libraries) {
             $scope.libs = libraries.data;
         });
 
         $scope.useScript = function (name) {
             $scope.id = name;
+            console.log('NAME CLICK', name);
         };
+
+        $scope.bundle = function (query) {
+            $location.path('/' + query);
+        };
+
+        if ($route.current.params.names) {
+            $timeout(function () {
+                var savedLibs = $route.current.params.names.split('+');
+                angular.forEach(savedLibs, function (savedLib) {
+                    angular.element('.' + $filter('noSpace')(savedLib)).trigger("click");
+                });
+            }, 500, true);
+        }
+
     }]);
